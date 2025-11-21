59b347ab07e76001c25206c82116c185d769e703
Mariyababu Dasari <Mariyababu.Dasari@enreap.com>
2025-11-21 17:26:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# nodejs-app-mss
+
+To start this application first you can get this repo code using below url
+
+git clone https://github.com/MithunTechnologiesDevOps/nodejs-app-mss.git
+
+cd nodejs-app-mss
+
+npm install
+
+node app.js 
+
+(OR) 
+
+npm start
+
+To Execute the SonarQube Repor, execute the below command.
+
+npm run sonar
+
+(OR) 
+
+node sonar-project.js
+
+to upload into nexus repo, user below command
+npm publish
+
+To generate the Nexus token with base 64 format, we will use below command
+
+echo -n "username:password" | openssl base64
+
+
---
+++ sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://3.216.226.173:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'node.js-app',
+	    'sonar.projectKey':'node.js-app',
+	    'sonar.login': 'squ_b7734331ca6f86807aaeec4b64fbc99f5cbef778',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
